f372281a83d0fb491546b3da4d43ed8d8ae0d933
ehancup <mondokskuy19@gmail.com>
2023-02-01 07:24:44 +0700

    dark theme maintain and auto swiper

--- nav.js
+++ nav.js
@@ -1,6 +1,7 @@
 const btnMenu = document.querySelector('.btn-menu')
 const navMenu = document.querySelector('.nav')
 const clsMenu = document.querySelector('.btn-close')
+const main = document.querySelectorAll('.main')
 
 btnMenu.addEventListener('click', function() {
     navMenu.classList.toggle('nav-active')
@@ -12,4 +13,10 @@ clsMenu.addEventListener('click', function() {
     navMenu.classList.toggle('nav-active')
     btnMenu.classList.toggle('btn-active')
     clsMenu.classList.toggle('boton')
+})
+
+main.addEventListener('scroll', function() {
+    navMenu.classList.remove('nav-active')
+    btnMenu.classList.remove('btn-active')
+    clsMenu.classList.remove('boton')
 })
